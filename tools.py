2d78b7356e18cb18e768b81f1f48d8312aa41a9a
basant <basantprajapati2013@gmail.com>
2026-09-22 15:43:14 +0530

    modified tools.py to tools and ToolNode code

--- tools.py
+++ tools.py
@@ -1,5 +1,16 @@
 from retriever import retriever
-
+from langchain_core.tools import create_retriever_tool
+from langgraph.prebuilt import ToolNode
 print("Loading retriever...")
 retriever = retriever()
-print("Retriever loaded successfully.\n", retriever)
+
+retriever_tool = create_retriever_tool(
+    retriever,
+    "retrieve_",
+    "Search and return information related to query. You are a specialized assistant. Use the 'retriever_tool' **only** when the query explicitly relates to LangChain blog data. For all other queries, respond directly without using any tool. For simple queries like 'hi', 'hello', or 'how are you', provide a normal response.",   
+)
+
+
+retrieve = ToolNode([retriever_tool])
+
+tools = [retriever_tool]
